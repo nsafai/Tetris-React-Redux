6d4365bcdcb90a5f113a0e9fa06cc6602ccbc07d
Nicolai Safai <nicolaisafai@gmail.com>
2019-04-06 19:41:09 -0700

    connected control buttons to redux

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 
 class Controls extends Component {
 
   render() {
+    const { isRunning } = this.props
+
     return (
       <div className="controls">
         {/* left */}
         <button className="control-button" onClick={(e) => {
-
-        }}>⬅</button>
+          if (!isRunning) { return }
+          this.props.moveLeft()
+        }}>⇦</button>
 
         {/* right */}
         <button className="control-button" onClick={(e) => {
-
-        }}>➡</button>
+          if (!isRunning) { return }
+          this.props.moveRight()
+        }}>⇨</button>
 
         {/* rotate */}
         <button className="control-button" onClick={(e) => {
-
+          if (!isRunning) { return }
+          this.props.rotate()
         }}>⤿</button>
-
+          
         {/* down */}
         <button className="control-button" onClick={(e) => {
-
-        }}> ⬇</button>
+          if (!isRunning) { return }
+          this.props.moveDown()
+        }}>⇩</button>
 
       </div>
     )
   }
 }
 
-export default Controls
+const mapStateToProps = (state) => {
+  return {
+    isRunning: state.game.isRunning
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    moveRight,
+    moveLeft,
+    moveDown,
+    rotate
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(Controls)
+// export default Controls
